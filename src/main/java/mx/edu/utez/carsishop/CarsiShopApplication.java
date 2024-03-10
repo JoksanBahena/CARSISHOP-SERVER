f855e7f20759641c8044cf6ddea0b6b9da0976ec
DavidRos1231 <20213tn038@utez.edu.mx>
2024-03-09 19:31:34 -0600

    Creacion del backEnd del proyecto de tienda

---
+++ src/main/java/mx/edu/utez/carsishop/CarsiShopApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.carsishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsiShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsiShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/carsishop/CarsiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.carsishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsiShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
