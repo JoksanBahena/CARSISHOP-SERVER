df57d5f9fde9ba4447b06777188ef32f49398179
JosafatMv <20213tn124@utez.edu.mx>
2024-03-30 20:41:40 -0600

    [FIX] Role admin para el usuario default

--- src/main/java/mx/edu/utez/carsishop/utils/InitialScript.java
+++ src/main/java/mx/edu/utez/carsishop/utils/InitialScript.java
@@ -2,10 +2,12 @@ package mx.edu.utez.carsishop.utils;
 
 import mx.edu.utez.carsishop.Auth.AuthService;
 import mx.edu.utez.carsishop.Auth.RegisterRequest;
+import mx.edu.utez.carsishop.models.user.Role;
 import mx.edu.utez.carsishop.models.user.User;
 import mx.edu.utez.carsishop.models.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -13,29 +15,30 @@ import java.util.Optional;
 @Component
 public class InitialScript implements CommandLineRunner {
 
-    private final AuthService authService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public InitialScript(AuthService authService, UserRepository userRepository) {
-        this.authService = authService;
+    public InitialScript(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args)  {
 
-        Optional<User> user = userRepository.findByUsername("admin@gmail.com");
-        if (!user.isPresent()) {
-            RegisterRequest registerRequest = new RegisterRequest();
+        Optional<User> userExist = userRepository.findByUsername("admin@gmail.com");
+        if (!userExist.isPresent()) {
+            User user = User.builder()
+                    .username("admin@gmail.com")
+                    .password(passwordEncoder.encode( "admin"))
+                    .name("admin")
+                    .surname("admin")
+                    .phone("1234567890")
+                    .role(Role.ADMIN)
+                    .build();
 
-            registerRequest.setEmail("admin@gmail.com");
-            registerRequest.setPassword("admin");
-            registerRequest.setName("admin");
-            registerRequest.setSurname("admin");
-            registerRequest.setPhone("1234567890");
-
-            authService.register(registerRequest);
+            userRepository.save(user);
         }
 
 
