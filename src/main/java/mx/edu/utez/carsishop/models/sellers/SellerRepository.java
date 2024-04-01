75d1cfa0390a6746602c23b5ffcf6976bdc18d2d
JoksanBahena <20203tn008@utez.edu.mx>
2024-03-31 20:28:53 -0600

    [ADD] Modulo vendedor
    Se agrego metodos para registro de vendedor y actualizar su info

--- src/main/java/mx/edu/utez/carsishop/models/sellers/SellerRepository.java
+++ src/main/java/mx/edu/utez/carsishop/models/sellers/SellerRepository.java
@@ -2,5 +2,10 @@ package mx.edu.utez.carsishop.models.sellers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+    boolean existsByRfc (String rfc);
+    boolean existsByCurp (String curp);
+    Optional<Seller> findByUserId (Long idUser);
 }
