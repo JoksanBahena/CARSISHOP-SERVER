5a42f63afc152df4cc7a5dd1a2638d5e0ce57df6
DavidRos1231 <20213tn038@utez.edu.mx>
2024-04-10 23:27:14 -0600

    [ADD] Se agrego un ejemplo de la intercepcion del token en UserController para su uso en los servicios

--- src/main/java/mx/edu/utez/carsishop/controllers/user/UserController.java
+++ src/main/java/mx/edu/utez/carsishop/controllers/user/UserController.java
@@ -25,4 +25,20 @@ public class UserController {
         return userService.updateUserInfo(userDto);
     }
 
+    @GetMapping("/getBearer")
+    public ResponseEntity<CustomResponse<String>> updateInfo(@RequestHeader("Authorization") String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            // Extraemos el token JWT eliminando el prefijo "Bearer "
+            String jwtToken = authorizationHeader.substring(7);
+
+            // Ahora puedes utilizar el token JWT de la forma que necesites
+            // Por ejemplo, puedes validar el token, extraer información del usuario, etc.
+
+            return ResponseEntity.ok(new CustomResponse<>(jwtToken,false,200,"hola",1));
+        } else {
+            // Si no se proporciona el token en el encabezado de autorización, puedes manejar el caso aquí
+            return ResponseEntity.ok(new CustomResponse<>(null,true,400,"ctm",1));
+
+        }
+    }
 }
