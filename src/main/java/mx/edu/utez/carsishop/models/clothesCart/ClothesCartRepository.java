e411b78e1390722c1915f11ab0e21fbc44d70957
DavidRos1231 <20213tn038@utez.edu.mx>
2024-04-12 00:32:45 -0600

    [UPDATE] Arreglo en el ClothesRepository

--- src/main/java/mx/edu/utez/carsishop/models/clothesCart/ClothesCartRepository.java
+++ src/main/java/mx/edu/utez/carsishop/models/clothesCart/ClothesCartRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ClothesCartRepository extends JpaRepository<ClothesCart, Long> {
-    List<ClothesCart> findByUser(User user);
 }
