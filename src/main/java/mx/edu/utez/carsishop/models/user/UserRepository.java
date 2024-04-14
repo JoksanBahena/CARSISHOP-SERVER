a5ee8a843060066d996bbe06ca2c1ebf8322d862
EmilianoChofis <20213tn128@utez.edu.mx>
2024-04-14 14:05:05 -0600

    [FIX] fix importación

--- src/main/java/mx/edu/utez/carsishop/models/user/UserRepository.java
+++ src/main/java/mx/edu/utez/carsishop/models/user/UserRepository.java
@@ -1,5 +1,6 @@
 package mx.edu.utez.carsishop.models.user;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
