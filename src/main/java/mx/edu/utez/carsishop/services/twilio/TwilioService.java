742b958a223a7340f2d66e23798e203692531f0e
DavidRos1231 <20213tn038@utez.edu.mx>
2024-04-17 23:30:22 -0600

    [FIX] Se cubrio el metodo de twilio con un try

--- src/main/java/mx/edu/utez/carsishop/services/twilio/TwilioService.java
+++ src/main/java/mx/edu/utez/carsishop/services/twilio/TwilioService.java
@@ -1,10 +1,8 @@
 package mx.edu.utez.carsishop.services.twilio;
 
 import com.twilio.Twilio;
-import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import mx.edu.utez.carsishop.models.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +16,7 @@ public class TwilioService {
     @Value("${TWILIO_ACCOUNT_PHONE}")
     String phoneNumber;
 
-    public void sendSMS(String msj) throws ApiException {
+    public void sendSMS(String msj)  {
         Twilio.init(sid, tokenTw);
 
         Message.creator(new PhoneNumber("whatsapp:+5217774239270"),
